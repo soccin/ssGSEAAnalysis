135585004b54893a3fc6da521008990c3ab4d84b
Nicholas D. Socci <soccin@mskcc.org>
2023-09-10 21:20:21 -0400

    tsv2gct function complete

--- tsv2gct.R
+++ tsv2gct.R
@@ -1,12 +1,19 @@
-tsv2gct<-function(tbl) {
+tsv2gct<-function(tbl,file_name) {
 
+    line2 <- paste(nrow(tbl),ncol(tbl)-1,sep='\t')
 
-    line1<-'#1.2'
-    line2<-paste(num_rows,num_perm,sep='\t')
-    line3<-paste('X',1:num_perm,sep='',collapse='\t')
-    line3<-paste('NAME','Description',line3, sep='\t')
-    write.table(line1, paste(profile_data_path,'/random_profile_',profile_data_name,'.gct',sep=""),quote=F,col.name=F,row.name=F) #first line for the gct file
-    write.table(line2, paste(profile_data_path,'/random_profile_',profile_data_name,'.gct',sep=""),quote=F,col.name=F,row.name=F,append=T)  # 2nd line for the gct file
-    write.table(line3, paste(profile_data_path,'/random_profile_',profile_data_name,'.gct',sep=""),quote=F,col.name=F,row.name=F,append=T)  # 2nd line for the gct file
+    gct_tbl <- tbl %>%
+        rename(NAME=1) %>%
+        mutate(Description=NAME) %>%
+        select(NAME,Description,everything())
+
+    #
+    # first line of gct is version comment must be "#1.2"
+    # second line is rows, cols (data cols)
+    #
+    write.table("#1.2", file_name, quote=F, col.name=F, row.name=F)
+    write.table(line2, file_name, quote=F, col.name=F, row.name=F, append=T)
+    write.table(gct_tbl, file_name, quote=F, col.name=F, row.name=F, append=T)
 
 }
+
