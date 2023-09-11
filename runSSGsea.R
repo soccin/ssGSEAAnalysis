bfc01c4841377b57371372c4313420ebe0f0d1e3
Nicholas D. Socci <soccin@mskcc.org>
2023-09-10 21:55:19 -0400

    checkpoint

--- runSSGsea.R
+++ runSSGsea.R
@@ -1,4 +1,18 @@
 source("tsv2gct.R")
-dd=readr::read_csv("../cc_normcounts.csv")
-tsv2gct(dd,"cc_normcounts.gct")
+
+FILE="../cc_normcounts.csv"
+GCT_FILE=gsub(".csv",".gct",basename(FILE))
+
+dd=readr::read_csv(FILE)
+tsv2gct(dd,GCT_FILE)
+
 require(ssgsea.GBM.classification)
+
+#
+# Do not run by default
+#
+num_perm=10
+if(0) {
+    runSsGSEAwithPermutation(GCT_FILE,num_perm)
+}
+
